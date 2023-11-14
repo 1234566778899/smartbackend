a088000aa3b992427db2884ae39d0ed6e1e8012c
Carlos Ordaz <carlos.ordaz@acomo.com.pe>
2023-11-13 20:23:48 -0500

    sin cors

--- src/index.js
+++ src/index.js
@@ -5,10 +5,7 @@ require('./models/db')
 app.use(express.json());
 const port = process.env.PORT || 4000;
 
-app.use(cors({
-    origin: ['https://smartbuy-fronted.vercel.app', 'http://localhost:3000'],
-    credentials: true
-}));
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('v.1.0.8')
