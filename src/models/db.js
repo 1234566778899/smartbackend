9f80360a6953177dfca8b2f71cc3c5ca895f23fa
Carlos Ordaz <carlos.ordaz@acomo.com.pe>
2023-11-13 21:48:23 -0500

    v12

--- src/models/db.js
+++ src/models/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
-mongoose.connect(process.env.MONGODB_URL, {
+mongoose.connect('mongodb://    smartbuydb:T8h3mu7tI4CzeImHhdikSzyPgtDSC7z9XbrXLe8fyDXVcBMIA5fsNYN7Kk6udHQcw6s0eYIdrAfKACDbFKeYzw==@smartbuydb.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&replicaSet=globaldb&maxIdleTimeMS=120000&appName=@smartbuydb@/smartbuy', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then(db => console.log('db connected'))
